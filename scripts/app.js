6a5ca89292870b845bd28d9333a9d5d5f13cec8e
Emad Rahimi <121079771+emadjs@users.noreply.github.com>
2023-10-05 07:32:16 +0330

    add header

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
-console.log("Hola")
+const navBtn = document.querySelector(".nav__btn")
+const navMobile = document.querySelector(".nav-mobile")
+
+navBtn.addEventListener("click", () => {
+    navBtn.classList.toggle("nav__btn--open")
+    navMobile.classList.toggle("nav-mobile--open")
+})
